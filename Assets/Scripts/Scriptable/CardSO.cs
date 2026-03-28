1502dbe5dfb156046d9b202f191d14db27857c6b
Labham Baheti <labham7@gmail.com>
2026-03-28 11:46:35 +0530

    Update CardSO CreateAssetMenu attributes
    Adjust the CreateAssetMenu settings for CardSO to use a specific fileName ('Card') and menuName ('ScriptableObjects/CardScriptableObject') instead of the previous generic values. This clarifies the asset creation entry in the Unity editor; no other code logic was changed.

--- Assets/Scripts/Scriptable/CardSO.cs
+++ Assets/Scripts/Scriptable/CardSO.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+[CreateAssetMenu(fileName = "Card", menuName = "ScriptableObjects/CardScriptableObject", order = 1)]
 public class CardSO : ScriptableObject
 {
     int Id;
