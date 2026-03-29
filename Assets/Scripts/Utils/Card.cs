d6d8bc405b9ecc8cf2f03cc28f6e7172e7669ea2
Labham Baheti <labham7@gmail.com>
2026-03-29 10:33:05 +0530

    Refactor Card flip logic and Init method
    Rename SetCardData to Init and move initial Unflip call from OnEnable into Init (remove OnEnable). Refactor flip coroutine to use helper methods: TryStartFlip, AnimateScale, SwapSprite and EndFlip for clearer flow and safer isFlipping handling. Replace direct access to CardManager.backSprite with CardManager.GetBackSprite(). Overall simplifies scale animation, sprite swapping and flip state management.

--- Assets/Scripts/Utils/Card.cs
+++ Assets/Scripts/Utils/Card.cs
@@ -6,31 +6,25 @@ using UnityEngine.EventSystems;
 public class Card : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private Image cardSprite;
-
-    //can be set from Cardmanager as this wil save the reference to sprite for each card
     [SerializeField] private Sprite frontSprite;
     public int id = -1;
 
     [SerializeField] private float flipDuration = 0.15f;
 
-    void OnEnable()
-    {
-        Unflip(null, 3f); // Start with cards face down
-    }
     public void OnPointerClick(PointerEventData eventData)
     {
         if (isFlipping) return;
 
         Flip(OnFlipComplete);
     }
-    public void SetCardData(Sprite sprite, int cardId)
+    public void Init(Sprite sprite, int cardId)
     {
         id = cardId;
         cardSprite.sprite = sprite;
         frontSprite = sprite;
+        Unflip(null, 3f);
+
     }
-
-
 
     private bool isFlipping = false;
 
@@ -50,42 +44,51 @@ public class Card : MonoBehaviour, IPointerClickHandler
         if (delay > 0f)
             yield return new WaitForSeconds(delay);
 
-        if (isFlipping)
+        if (!TryStartFlip())
             yield break;
 
-        isFlipping = true;
+        yield return AnimateScale(1f, 0f);
 
-        // Hide
-        for (float t = 0; t < flipDuration; t += Time.deltaTime)
-        {
-            float scale = Mathf.Lerp(1f, 0f, t / flipDuration);
-            transform.localScale = new Vector3(scale, 1f, 1f);
-            yield return null;
-        }
+        SwapSprite(showFront);
 
-        transform.localScale = new Vector3(0f, 1f, 1f);
+        yield return AnimateScale(0f, 1f);
 
-        // Swap
-        cardSprite.sprite = showFront ? frontSprite : CardManager.Instance.backSprite;
-
-        // Show
-        for (float t = 0; t < flipDuration; t += Time.deltaTime)
-        {
-            float scale = Mathf.Lerp(0f, 1f, t / flipDuration);
-            transform.localScale = new Vector3(scale, 1f, 1f);
-            yield return null;
-        }
-
-        transform.localScale = Vector3.one;
-
-        isFlipping = false;
-
-
-        onComplete?.Invoke(this);
+        EndFlip(onComplete);
     }
 
+    private bool TryStartFlip()
+    {
+        if (isFlipping) return false;
+
+        isFlipping = true;
+        return true;
+    }
+
+    private IEnumerator AnimateScale(float from, float to)
+    {
+        for (float t = 0; t < flipDuration; t += Time.deltaTime)
+        {
+            float scale = Mathf.Lerp(from, to, t / flipDuration);
+            transform.localScale = new Vector3(scale, 1f, 1f);
+            yield return null;
+        }
+
+        transform.localScale = new Vector3(to, 1f, 1f);
+    }
+
+    private void SwapSprite(bool showFront)
+    {
+        cardSprite.sprite = showFront ? frontSprite : CardManager.Instance.GetBackSprite();
+    }
+
+    private void EndFlip(System.Action<Card> onComplete)
+    {
+        isFlipping = false;
+        onComplete?.Invoke(this);
+    }
     private void OnFlipComplete(Card card)
     {
+        Debug.Log("Flip Complete: " + card.id);
         CardManager.OnCardClicked?.Invoke(card);
     }
 
